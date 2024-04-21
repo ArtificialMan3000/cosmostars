608afcca3be7cf158f121077683e343ba7d19eb7
Alexander <legionerab3000@yandex.ru>
2024-04-21 15:22:48 +0300

    Add deleting old database files while reinitialization project

--- init.js
+++ init.js
@@ -3,4 +3,6 @@ const fs = require('fs')
 fs.copyFileSync('.env.sample', '.env')
 fs.copyFileSync('.env.sample', 'packages/server/.env')
 
+// Remove old database files
+fs.rmSync("tmp/pgdata", { recursive: true, force: true });
 fs.mkdirSync('tmp/pgdata', { recursive: true })
