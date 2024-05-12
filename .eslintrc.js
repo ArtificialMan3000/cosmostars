87b2fb01196ff6a011459eb83e2f33e0ff3edd63
Alexander <legionerab3000@yandex.ru>
2024-05-12 07:24:49 +0300

    FIX .eslintrc.js for different processing of js and ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,24 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 11,
   },
-  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  plugins: ['simple-import-sort'],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 1,
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 1,
+      },
+    },
+  ],
 };
